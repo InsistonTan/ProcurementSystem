30207b1f03e8615616826581014cdb4571652cbd
InsistonTan <1763344790@qq.com>
2020-07-08 18:29:29 +0800

    first commit

---
+++ src/main/java/com/huiduoduo/procurement_system/ProcurementSystemApplication.java
@@ -0,0 +1,15 @@
+package com.huiduoduo.procurement_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcurementSystemApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ProcurementSystemApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/huiduoduo/procurement_system/controller/MainController.java
@@ -0,0 +1,26 @@
+package com.huiduoduo.procurement_system.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author TanJifeng
+ * @Description
+ * @date 2020/7/8 11:48
+ */
+@Controller
+public class MainController {
+
+    //跳转到首页
+    @RequestMapping("/")
+    public void index(HttpServletRequest request,HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        request.getRequestDispatcher("/index.html").forward(request,response);
+    }
+}
