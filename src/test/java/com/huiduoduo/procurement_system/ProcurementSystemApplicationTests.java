12318dfecaa7ce9307f575f7ae3d70fc3f6970ef
InsistonTan <1763344790@qq.com>
2020-07-08 18:29:29 +0800

    first commit

---
+++ src/test/java/com/huiduoduo/procurement_system/ProcurementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huiduoduo.procurement_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcurementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
