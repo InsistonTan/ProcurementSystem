a279f9600ba1314750e7a8956ab92c68ea52e746
InsistonTan <1763344790@qq.com>
2020-08-17 22:43:38 +0800

    增加AccountController

--- src/main/java/com/huiduoduo/ProcurementSystem/controller/AccountController.java
+++ src/main/java/com/huiduoduo/ProcurementSystem/controller/AccountController.java
@@ -1,14 +1,16 @@
 package com.huiduoduo.ProcurementSystem.controller;
-
 import com.huiduoduo.ProcurementSystem.domain.Account;
 import com.huiduoduo.ProcurementSystem.service.AccountService;
+import com.huiduoduo.ProcurementSystem.utils.ResultUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -30,13 +32,11 @@ public class AccountController {
     @RequestMapping("/login")
     @ResponseBody
     public Map login(@RequestBody Account account){
-        Map result=new HashMap();
+        Map result;
         //检查登录信息
-        if(account==null||account.getUsername()==null||account.getPassword()==null||account.getRole()==null){
-            result.put("status","failed");
-            result.put("msg","登陆信息中存在空信息");
-            return result;
-        }
+        System.out.println(account);
+        if(account==null||account.getUsername()==null||account.getPassword()==null||account.getRole()==null)
+            return ResultUtil.getErrorRes("登陆信息中存在空信息");
         //登陆
         result=accountService.login(account);
         //获取登陆结果状态
@@ -48,6 +48,7 @@ public class AccountController {
             request.getSession().setAttribute("username",info.getUsername());
             request.getSession().setAttribute("name",info.getName());
             request.getSession().setAttribute("role",info.getRole());
+            request.getSession().setAttribute("info",info);
         }
         return result;
     }
@@ -59,16 +60,65 @@ public class AccountController {
         Map result=new HashMap();
         //检查权限
         String role=(String) request.getSession().getAttribute("role");
-        if(!"admin".equalsIgnoreCase(role)){
-            result.put("status","failed");
-            result.put("msg","没有权限进行此操作");
-            return result;
-        }
+        if(!"admin".equalsIgnoreCase(role))
+            return ResultUtil.getErrorRes("没有权限进行此操作");
         //
-        List<Account> data=accountService.selectAll();
-        result.put("status","success");
-        result.put("data",data);
-        return result;
+        try{
+            List<Account> data=accountService.selectAll();
+            result.put("status","success");
+            result.put("data",data);
+            return result;
+        }catch (Exception e){
+            return ResultUtil.getErrorRes("添加进数据库失败");
+        }
+
         //System.out.println(res);
+    }
+
+    //（管理员）增加用户
+    @RequestMapping("/setUser")
+    @ResponseBody
+    public Map setUser(@RequestBody Account account){
+        Map result;
+        //检查登陆用户权限
+        String role=(String) request.getSession().getAttribute("role");
+        //不是管理员
+        if(!"admin".equalsIgnoreCase(role))
+            return ResultUtil.getErrorRes("没有权限进行此操作");
+
+        //是管理员
+        result=accountService.addAccount(account);
+        return result;
+    }
+
+    //（管理员）删除用户
+    @RequestMapping("/deleteUser")
+    @ResponseBody
+    public Map deleteUser(@RequestBody Account account){
+        Map result;
+        //检查登陆用户权限
+        String role=(String) request.getSession().getAttribute("role");
+        //不是管理员
+        if(!"admin".equalsIgnoreCase(role))
+            return ResultUtil.getErrorRes("没有权限进行此操作");
+
+        //是管理员
+        result=accountService.deleteAccount(account);
+        //返回结果
+        return result;
+    }
+
+    //修改用户信息
+    @RequestMapping("/updateUser")
+    @ResponseBody
+    public Map updateUser(@RequestBody Account account){
+        Map result;
+        //检查登陆
+        Object obj_username=request.getSession().getAttribute("username");
+        if(obj_username==null)
+            return ResultUtil.getErrorRes("你还没有登陆");
+        //
+        result=accountService.updateAccount(account);
+        return result;
     }
 }
--- src/main/java/com/huiduoduo/ProcurementSystem/controller/MainController.java
+++ src/main/java/com/huiduoduo/ProcurementSystem/controller/MainController.java
@@ -1,24 +1,24 @@
-//package com.huiduoduo.ProcurementSystem.controller;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import javax.servlet.ServletException;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//
-///**
-// * @author TanJifeng
-// * @Description
-// * @date 2020/7/8 11:48
-// */
-//@Controller
-//public class MainController {
-//
-//    //跳转到首页
-//    @RequestMapping("/")
-//    public void index(HttpServletRequest request,HttpServletResponse response)
-//            throws ServletException, IOException
-//    {
-//        request.getRequestDispatcher("/index.html").forward(request,response);
-//    }
-//}
+package com.huiduoduo.ProcurementSystem.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author TanJifeng
+ * @Description
+ * @date 2020/7/8 11:48
+ */
+@Controller
+public class MainController {
+
+    //跳转到首页
+    @RequestMapping("/")
+    public void index(HttpServletRequest request,HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        request.getRequestDispatcher("/index.html").forward(request,response);
+    }
+}
