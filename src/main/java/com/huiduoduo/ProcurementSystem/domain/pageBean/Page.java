b6477f42ccb521275f430da35fbc2c2b8e45c248
InsistonTan <1763344790@qq.com>
2020-09-10 18:15:52 +0800

    修改Page的属性名

--- src/main/java/com/huiduoduo/ProcurementSystem/domain/pageBean/Page.java
+++ src/main/java/com/huiduoduo/ProcurementSystem/domain/pageBean/Page.java
@@ -9,7 +9,7 @@ import java.io.Serializable;
  */
 @Data
 public class Page implements Serializable {
-    protected int pageNum;//当前页码
-    protected int pageSize;//页的大小
+    protected int page;//当前页码
+    protected int limit;//页的大小
     protected String sort;//升序或降序，"+id"为升序
 }
